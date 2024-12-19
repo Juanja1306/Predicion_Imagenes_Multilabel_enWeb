0963167d5939a554fd92ab490cfbfebb00e5adba
Juanja1306 <juanjamalovega@gmail.com>
2024-12-19 13:49:00 -0500

    Actualizado a resnet50

--- backend/prediccion/views.py
+++ backend/prediccion/views.py
@@ -10,7 +10,7 @@ def predict_image(request):
         image_path = request.FILES.get('image')
         if image_path:
             # Asigna el path adecuado al archivo recibido o maneja directamente el objeto imagen
-            model = MLC(num_classes=78)  # Aquí podrías cargar el modelo solo una vez al iniciar el servidor en otro escenario
+            model = MLC(num_classes=80)  # Aquí podrías cargar el modelo solo una vez al iniciar el servidor en otro escenario
             predicted_labels = predict_and_show(image_path, model)
             return JsonResponse({'predictions': predicted_labels})
         return JsonResponse({'error': 'No image provided'}, status=400)
